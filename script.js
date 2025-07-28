f9e8ef304061b759dc3a493831ac506096656067
Eman Tariq <emantariq.1004@gmail.com>
2025-07-28 14:13:37 +0500

    final commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let voices = [];
 
 let voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () => {
+window.speechSynthesis.onvoiceschanged = () => { //window.speechSynthesis = a browser feature that allows you to use text-to-speech
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
 
@@ -19,5 +19,5 @@ voiceSelect.addEventListener("change", () => {
 
 document.querySelector("button").addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
+  window.speechSynthesis.speak(speech);  //Tells the browser to speak the text using the selected voice.
 });
